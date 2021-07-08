8aac551853193c73319e5f708f33c67f7129331a
Rauf Maharramov <raufmaharramov@Raufs-MacBook-Pro.local>
2021-07-08 07:39:53 -0400

    bug fixs

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
-const path = require("path");
-const app = express();
+// const express = require("express");
+// require("./db/mongoose");
+// const userRouter = require("./routers/user");
+// const taskRouter = require("./routers/task");
+// const path = require("path");
+// const app = express();
 
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
+// app.use(express.json());
+// app.use(userRouter);
+// app.use(taskRouter);
 
-// Set the static folder
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// // Set the static folder
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
-module.exports = app;
+// module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-const app = require("./app");
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+// Set the static folder
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
