23c85b95734adc582a46bdc74d4e11b16bbdb4bf
Rauf Maharramov <raufmaharramov@Raufs-MacBook-Pro.local>
2021-07-08 06:54:43 -0400

    deployment

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+// Set the static folder
 if (process.env.NODE_ENV === "production") {
-  // Set the static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
