6f5fd4f30025a15052e9f48a6e538d6b53594410
Rauf Maharramov <raufmaharramov@Raufs-MacBook-Pro.local>
2021-07-08 10:16:44 -0400

    add v

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
-const proxy = require("http-proxy-middleware");
 const path = require("path");
 const app = express();
 
@@ -10,20 +9,15 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-module.exports = function (app) {
-  // add other server routes to path array
-  app.use(proxy(["/"], { target: "http://localhost:5000" }));
-};
-
 // Set the static folder
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+if (process.env.NODE_ENV === "production") {
   // Add production middleware such as redirecting to https
 
   // Express will serve up production assets i.e. main.js
-  app.use("/static", express.static(path.join(`${__dirname}/client/build`)));
+  app.use(express.static("client/build"));
   // If Express doesn't recognize route serve index.html
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/build/`));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
