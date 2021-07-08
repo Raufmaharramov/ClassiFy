ac6c49f1acbb8636957a7a49207e3f95493e604a
Rauf Maharramov <raufmaharramov@Raufs-MacBook-Pro.local>
2021-07-08 09:26:15 -0400

    fixing.

--- src/index.js
+++ src/index.js
@@ -10,26 +10,24 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+module.exports = function (app) {
+  // add other server routes to path array
+  app.use(proxy(["/api"], { target: "http://localhost:5000" }));
+};
+
 // Set the static folder
 if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
   // Add production middleware such as redirecting to https
 
   // Express will serve up production assets i.e. main.js
-  app.use(express.static(__dirname + "/client/build"));
+  app.use(express.static("client/build"));
   // If Express doesn't recognize route serve index.html
-  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-module.exports = function (app) {
-  // add other server routes to path array
-  app.use(proxy(["/"], { target: "http://localhost:5000" }));
-};
